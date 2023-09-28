e8e4d2bbbd160dc41efa65a7de8edad412d23d9e
Shama <assiddik001@gmail.com>
2023-09-28 07:38:20 +0600

    layout

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+{
+  /* <UserButton afterSignOutUrl="/" /> */
+}
 
-const Dashboard = () => {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+const Dashboard = ({ children }: { children: React.ReactNode }) => {
+  return <div></div>;
 };
 
 export default Dashboard;
